ee4e1e81a1f68a7a9fecb7983890936a3fccb29c
18959222105 <41536060+18959222105@users.noreply.github.com>
2019-10-15 18:46:05 +0800

    Add files via upload

---
+++ pages/play/play.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<audio :src=url controls="controls"></audio>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url:""
+			}
+		},
+		onLoad(e){
+			console.log("1111")
+			console.log(e)
+			this.url='http://yiapi.xinli001.com/fm/media-url.mp3?id='+e.id
+			console.log(this.url)
+		},
+		
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
